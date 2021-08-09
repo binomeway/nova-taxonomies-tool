cfba968a1faa3dade5d03a32a3f7e4048f4500ad
Codrin Axinte <codrin.axinte@outlook.com>
2021-08-09 12:46:39 +0300

    Update navigation.blade.php

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -1,6 +1,34 @@
-<router-link tag="h3" :to="{name: 'nova-taxonomies-tool'}" class="cursor-pointer flex items-center font-normal dim text-white mb-6 text-base no-underline">
-    <svg class="sidebar-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="var(--sidebar-icon)" d="M3 1h4c1.1045695 0 2 .8954305 2 2v4c0 1.1045695-.8954305 2-2 2H3c-1.1045695 0-2-.8954305-2-2V3c0-1.1045695.8954305-2 2-2zm0 2v4h4V3H3zm10-2h4c1.1045695 0 2 .8954305 2 2v4c0 1.1045695-.8954305 2-2 2h-4c-1.1045695 0-2-.8954305-2-2V3c0-1.1045695.8954305-2 2-2zm0 2v4h4V3h-4zM3 11h4c1.1045695 0 2 .8954305 2 2v4c0 1.1045695-.8954305 2-2 2H3c-1.1045695 0-2-.8954305-2-2v-4c0-1.1045695.8954305-2 2-2zm0 2v4h4v-4H3zm10-2h4c1.1045695 0 2 .8954305 2 2v4c0 1.1045695-.8954305 2-2 2h-4c-1.1045695 0-2-.8954305-2-2v-4c0-1.1045695.8954305-2 2-2zm0 2v4h4v-4h-4z"/></svg>
+<h3 class="flex items-center font-normal text-white mb-6 text-base no-underline">
+    <svg class="sidebar-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path  stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z" />
+    </svg>
+
     <span class="sidebar-label">
-        Nova Taxonomies Tool
+        {{ __('Taxonomies') }}
     </span>
-</router-link>
+</h3>
+
+<ul class="list-reset mb-8">
+    <li class="leading-wide mb-4 text-sm">
+        <router-link :to="{
+        name: 'index',
+        params: {
+            resourceName: 'taxonomy-types'
+        }
+        }" class="text-white ml-8 no-underline dim">
+            {{ __('Types') }}
+        </router-link>
+    </li>
+
+    <li class="leading-wide mb-4 text-sm">
+        <router-link :to="{
+        name: 'index',
+        params: {
+            resourceName: 'tags'
+        }
+        }" class="text-white ml-8 no-underline dim">
+            {{ __('Tags') }}
+        </router-link>
+    </li>
+
+</ul>
