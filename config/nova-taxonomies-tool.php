473c7986ebdff4b0a0bd29e5f25b56a213243b80
Codrin Axinte <codrin.axinte@outlook.com>
2021-08-22 20:42:54 +0200

    Create nova-taxonomies-tool.php

---
+++ config/nova-taxonomies-tool.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    /**
+     * Cache options for taxonomies types.
+     */
+    'taxonomies_cache' => [
+        'key' => 'nova-taxonomies-types',
+        'ttl' => 30, // time to live in seconds
+    ],
+
+];
